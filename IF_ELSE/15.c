345cffae527b4c63c5a45841972a10d6aaf42bb0
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-05-11 02:05:28 +0530

    Add files via upload

---
+++ IF_ELSE/15.c
@@ -0,0 +1,47 @@
+//WAP to input week number and print week days.
+
+#include<stdio.h>
+int main()
+{
+    int w;
+    printf("Enter the Weak=");
+    scanf("%d",&w);
+
+    if(w==1)
+    {
+        printf("it's MONDAY");
+    }
+    else
+    if(w==2)
+    {
+        printf("it's TUESDAY");
+    }
+    else
+    if(w==3)
+    {
+    printf("it's WEDNESDAY");
+    }
+    else
+    if(w==4)
+    {
+        printf("it's THURSDAY");
+    }
+    else
+    if(w==5)
+    {
+        printf("it's FRIDAY");
+    }
+    else
+    if(w==6)
+    {
+        printf("it's SATURDAY");
+    }
+    else
+    if(w==7)
+    {
+        printf("it's SUNDAY");
+    }
+
+    
+
+}
