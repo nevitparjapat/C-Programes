7daa6eb24aa44636c8a5c4e7f19f0868c3693000
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-05-14 09:19:08 +0530

    Add files via upload

--- Loop.c/1.c
+++ Loop.c/1.c
@@ -1,15 +1,12 @@
-// WAP to print the number tgrom 0 to N if n is given by user
+// print name in 5 time
 
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    printf("Enter the Number=");
-    scanf("%d",&n);
-    i=0;
-    while(i<n)
+    int i;
+    for(i=0;i<5;i++)
     {
-        printf("%d \n",i);
-        i++;
+        printf("Nevit Parjapati\n");
     }
+    return 0;
 }
