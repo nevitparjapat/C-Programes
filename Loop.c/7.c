0711a6f1e37571727da6943e41ff88cf45030a5b
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-05-20 00:34:16 +0530

    Add files via upload

---
+++ Loop.c/7.c
@@ -0,0 +1,18 @@
+//WAP which will print all even numbers in between 1 to 20.
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    
+    for(i=1;i<=20;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d \n", i);
+        }
+    }
+    
+
+
+}
