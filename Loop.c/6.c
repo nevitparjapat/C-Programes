225c95af38eff08354133b2acfb2713ef7ecbc9d
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-05-19 12:47:15 +0530

    Add files via upload

---
+++ Loop.c/6.c
@@ -0,0 +1,15 @@
+//WAP which will print all numbers from 50 to 1 in reverse order.
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=50;i>=0;i--)
+    {
+        printf("%d \n",i);
+    }
+    return 0;
+}                                                     
+                                                     
+
+                                                                 
